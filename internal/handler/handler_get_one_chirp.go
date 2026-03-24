7a92e3863241bfe6466ad0c6346c33a4a6f6f9ae
swissymissy <pnghi1606@gmail.com>
2026-03-24 15:18:12 -0400

    add get one chirp ap endpoint

--- internal/handler/handler_get_one_chirp.go
+++ internal/handler/handler_get_one_chirp.go
@@ -3,10 +3,37 @@ package handler
 import (
 	"net/http"
 	"fmt"
+	"errors"
+	"database/sql"
 
-
+	"github.com/google/uuid"
 )
 
 func (apicfg *ApiConfig) HandlerGetOneChirp( w http.ResponseWriter, r *http.Request) {
-	
+	// get chirp ID
+	chirpIDStr := r.PathValue("chirpsID")
+	chirpID, err := uuid.Parse(chirpIDStr)
+	if err != nil {
+		ResponseWithError(w, http.StatusBadRequest, "Invalid chirp ID")
+		return
+	}
+	// retrieve chirp from table
+	chirpInfo, err := apicfg.DB.GetOneChirp(r.Context(), chirpID)
+	if errors.Is(err, sql.ErrNoRows) {
+		fmt.Printf("Error getting row from chirps table: %s\n", err)
+		ResponseWithError(w, 404, "Chirp does not exist or deleted")
+		return
+	} else if err != nil {
+		fmt.Printf("Error getting row from chirps table: %s\n", err)
+		ResponseWithError(w, 500, "Unable to get chirp. Try again")
+		return
+	}
+
+	ResponseWithJSON(w, 200, CreatedChirp{
+		ID: chirpInfo.ID,
+		CreatedAt: chirpInfo.CreatedAt,
+		UpdatedAt: chirpInfo.UpdatedAt,
+		Body: chirpInfo.Body,
+		UserID: chirpInfo.UserID,
+	})
 }
