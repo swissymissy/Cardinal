68ea513210b2d68290e12aa35bfdcdec9f158b9e
swissymissy <pnghi1606@gmail.com>
2026-03-31 16:20:08 -0400

    add comment in create_chirp

--- internal/handler/handler_create_chirp.go
+++ internal/handler/handler_create_chirp.go
@@ -56,6 +56,7 @@ func (apicfg *ApiConfig) HandlerCreateChirp(w http.ResponseWriter, r *http.Reque
 		ResponseWithError(w, 500, msg)
 		return
 	}
+	// get username 
 	user, err := apicfg.DB.GetUserByID(r.Context(), createdChirp.UserID)
 	if err != nil {
 		fmt.Printf("Error fetching user: %s\n", err)
