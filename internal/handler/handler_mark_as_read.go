a5907309904522582d3253848da8a569780947f7
swissymissy <pnghi1606@gmail.com>
2026-04-01 18:09:02 -0400

    fix format

--- internal/handler/handler_mark_as_read.go
+++ internal/handler/handler_mark_as_read.go
@@ -1,8 +1,8 @@
-package handler 
+package handler
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 
 	"github.com/google/uuid"
 	"github.com/swissymissy/Cardinal/internal/auth"
@@ -29,12 +29,11 @@ func (apicfg *ApiConfig) HandlerMarkAllRead(w http.ResponseWriter, r *http.Reque
 	err = apicfg.DB.MarkAllAsRead(r.Context(), userID)
 	if err != nil {
 		fmt.Printf("Can't mark all notif as read: %s\n", err)
-		ResponseWithError(w, 500 , "Can't mark all as read. Try again")
+		ResponseWithError(w, 500, "Can't mark all as read. Try again")
 		return
 	}
 	w.WriteHeader(200)
 }
-
 
 func (apicfg *ApiConfig) HandlerMarkOneRead(w http.ResponseWriter, r *http.Request) {
 	// get user token
@@ -52,7 +51,7 @@ func (apicfg *ApiConfig) HandlerMarkOneRead(w http.ResponseWriter, r *http.Reque
 		return
 	}
 	notifIDStr := r.PathValue("notifID")
-	notifID , err := uuid.Parse(notifIDStr)
+	notifID, err := uuid.Parse(notifIDStr)
 	if err != nil {
 		fmt.Printf("Can't parse string to uuid")
 		ResponseWithError(w, http.StatusBadRequest, "Invalid notif ID")
@@ -60,7 +59,7 @@ func (apicfg *ApiConfig) HandlerMarkOneRead(w http.ResponseWriter, r *http.Reque
 	}
 	// mark one notification as read
 	err = apicfg.DB.MarkOneAsRead(r.Context(), database.MarkOneAsReadParams{
-		ID: notifID,
+		ID:       notifID,
 		Receiver: userID,
 	})
 	if err != nil {
